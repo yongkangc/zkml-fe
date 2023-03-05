c0871183c3c284718ad15f3149ea96089197cc9a
Chia Yong Kang <chiayongkang@hotmail.com>
2023-03-04 17:45:24 -0700

    progress bar

--- pages/upload.js
+++ pages/upload.js
@@ -4,6 +4,7 @@
 import Header from '@/components/Header'
 import { Magic } from 'magic-sdk'
 import { magic } from '@/lib/magic'
+import { useState } from 'react'
 
 import styles from '@/styles/Home.module.css'
 
@@ -18,8 +19,16 @@ import { use, useEffect } from 'react'
 
 export default function Upload() {
   const { address, isConnecting, isDisconnected, isConnected } = useAccount()
+  const [time, setTime] = useState(0)
+
+  // Set progress bar with 5 different stages
+  const [progress, setProgress] = useState(0)
+
+  // Time counter for progress bar
+  // after every 45 seconds, progress bar increases by 1
 
   useEffect(() => {
+    setProgress(0)
     try {
       const connectWallet = async () => {
         await magic.wallet.connectWithUI()
@@ -29,6 +38,26 @@ export default function Upload() {
       console.log(error)
     }
   }, [])
+
+  useEffect(() => {
+    // after every 1 second, time increases by 1
+    const timer = setInterval(() => {
+      setTime(time + 1)
+    }, 1000)
+
+    // after every 45 seconds, progress bar increases by 1
+    if (time != 0 && time % 27 === 0) {
+      // check if progress is less than 5
+      if (progress < 5) {
+        setProgress(progress + 1)
+      }
+    }
+    console.log(time, progress)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [time])
 
   const uploadONNXModel = () => {
     // Upload the ONNX model to the server
@@ -58,14 +87,14 @@ export default function Upload() {
           <div className="flex flex-col w-1/2">
             <div className="flex justify-between mb-1">
               <span className="text-base font-large text-bold">
-                45% complete ...
+                {20 * progress} % complete ...
               </span>
             </div>
             <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
               {/* Set this to dynamically load progress */}
               <div
                 className="bg-yellow-600 h-2.5 rounded-full"
-                style={{ width: '45%' }}
+                style={{ width: `${20 * progress}%` }}
               ></div>
             </div>
           </div>
